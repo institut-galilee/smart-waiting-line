4c14762b45118d463bc97c81d4f0b9391ab466b4
LEBSIRAHLAM <46717362+LEBSIRAHLAM@users.noreply.github.com>
2019-03-21 12:31:38 +0100

    Update salon (1).sql

--- Interface/salon (1).sql
+++ Interface/salon (1).sql
@@ -42,27 +42,6 @@ CREATE TABLE IF NOT EXISTS `client` (
 
 INSERT INTO `client` (`id`, `nom`, `mail`) VALUES
 (1, 'ahlam', 'ahlam@yahoo.com'),
-(2, 'fatah', 'fatah@jhhh'),
-(3, 'lalal', 'llala@lkjlkaj'),
-(4, 'Nom', 'e-m@il'),
-(5, 'Nom', 'e-m@il'),
-(6, 'Nom', 'e-m@il'),
-(7, 'Nom', 'e-m@il'),
-(8, 'lili', 'lili'),
-(9, 'lili', 'lili'),
-(10, 'Nom', 'e-m@il'),
-(11, '', ''),
-(12, 'ahlam', 'ahlam'),
-(13, 'ahlam', 'lebsir.ahlam@yahoo.com'),
-(14, 'fatah', 'fatahlarfi1991@gmail.com'),
-(15, 'chahine', 'm.fredj23@gmail.com'),
-(16, 'achraf', 'achrafbya.by@gmail.com'),
-(17, 'achraf', 'achrafbya.by@gmail.com'),
-(18, 'dreams', 'dreams_lebi@live.fr'),
-(19, 'ahlam', 'ljaljaljalaj'),
-(20, 'seddik', 'seddik@hotmail'),
-(21, 'ahlam', 'lebsir.ahlam@yahoo.com'),
-(22, 'ahlam', 'lebsir.ahlam@yahoo.com');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
