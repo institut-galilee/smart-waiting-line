cdfe6d25f00ec588797abc064bdff7ad97e0f618
larfifatah <46718634+larfifatah@users.noreply.github.com>
2019-05-06 21:13:56 +0200

    Create salon.sql

---
+++ src/base de donnee/salon.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 03 mai 2019 à 15:54
+-- Version du serveur :  5.7.21
+-- Version de PHP :  5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `salon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `NOM` varchar(50) NOT NULL,
+  `MAIL` varchar(50) NOT NULL,
+  `NUM_TEL` varchar(20) NOT NULL,
+  `COIFFEUR` varchar(20) NOT NULL,
+  PRIMARY KEY (`NOM`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`NOM`, `MAIL`, `NUM_TEL`, `COIFFEUR`) VALUES
+('fredjchahine', 'm.fredj23@gmail.com', '0760825157', 'Fatah'),
+('ben youssefachraf', 'achrafbya.by@gmail.com ', '0783643089', 'Fatah');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `faouzi`
+--
+
+DROP TABLE IF EXISTS `faouzi`;
+CREATE TABLE IF NOT EXISTS `faouzi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fatah`
+--
+
+DROP TABLE IF EXISTS `fatah`;
+CREATE TABLE IF NOT EXISTS `fatah` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mohamed`
+--
+
+DROP TABLE IF EXISTS `mohamed`;
+CREATE TABLE IF NOT EXISTS `mohamed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
